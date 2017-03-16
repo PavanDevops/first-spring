c30e234d44caf5152da8784f993bfc7eb9630369
Lenovo <Lenovo@DESKTOP-9CUMSRB>
2017-03-16 14:33:55 +0530

    Added Service Controller

--- src/main/java/com/first/springboot/Application.java
+++ src/main/java/com/first/springboot/Application.java
@@ -1,5 +1,5 @@
 package com.first.springboot;
-
+ 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
