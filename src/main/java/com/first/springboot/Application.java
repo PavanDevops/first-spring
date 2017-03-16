a601192e56912be6854c7b17f8780711e79bc57b
Lenovo <Lenovo@DESKTOP-9CUMSRB>
2017-03-16 14:38:41 +0530

    added

--- src/main/java/com/first/springboot/Application.java
+++ src/main/java/com/first/springboot/Application.java
@@ -3,12 +3,16 @@ package com.first.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+
 
 @SpringBootApplication
+@ComponentScan("com.first")
 public class Application {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
 
     }
 
